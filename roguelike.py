1319de49f0c3d93036656dcc972ecca5408ea6c4
Iwo <iwo.tyszkowski@gmail.com>
2017-04-27 11:26:54 +0200

    pushpush

--- roguelike.py
+++ roguelike.py
@@ -1,14 +1,14 @@
 import random
 import os   # for screen clearing
+import sys, tty, termios    # for getch() function
 
 
 def getch():    # WASD moving
-    import sys, tty, termios
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     try:
         tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
+        ch = sys.stdin.read(1)  # wczytaj z systemu, z podstawowego wejscia, jeden znak
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
@@ -41,43 +41,39 @@ def insert_player(board, x, y):
 
 
 def border_touch(x, y, height, width):  # if touch border - exit
-    if x == 0 or x == width or y == 0 or y == height-2:
+    if x == 0 or x == width-1 or y == 0 or y == height-1:
         exit()
 
 
+def moving(key_input, x, y):
+    if key_input == ("w"):
+        y -= 1
+    elif key_input == ("a"):
+        x -= 1
+    elif key_input == ("s"):
+        y += 1
+    elif key_input == ("d"):
+        x += 1
+    elif key_input == ("p"):
+        os.system('clear')
+        exit()
+    return x, y
+
+
 def main():
-    # width = int(input('Enter map width: '))
-    # height = int(input('Enter map height: '))
+    os.system('clear')
     width = 50
     height = 20
-    # position_x = list(range(1, width-1))
-    # position_y = list(range(1, height-1))
-    # x = random.choice(position_x)
-    # y = random.choice(position_y)
     x = 5
     y = 5
 
     while True:
         print_board(insert_player(create_board(width, height), x, y))
         key_input = getch()
-        print(key_input)
-        if key_input == ("w"):
-            y -= 1
-            #border_touch()
-        elif key_input == ("a"):
-            x -= 1
-            #border_touch()
-        elif key_input == ("s"):
-            y += 1
-            #border_touch()
-        elif key_input == ("d"):
-            x += 1
-            #border_touch()
-        elif key_input == ("p"):
-            os.system('clear')
-            exit()
+        x, y = moving(key_input, x, y)
         border_touch(x, y, height, width)
         os.system('clear')
+
 
 if __name__ == '__main__':
     main()
